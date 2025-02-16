03a8a80763859cd84e472d329d30b7d8dcbf3f1f
Aljawharah004 <444200750@student.ksu.edu>
2025-02-16 15:43:38 +0300

    Method names update

---
+++ JavaApplication65/src/Csc227.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author asus
+ */
+public class Csc227 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
